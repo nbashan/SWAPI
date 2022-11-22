ad0f2eec57acb6ec9fd2fd772bf19c1eae885c3f
נתנאל בשן <nbashan@g.jct.ac.il>
2022-11-22 22:04:10 +0200

    create header css component

---
+++ assignment/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import '../App.css'
+
+const Header = () => {
+    return (
+        <header className="card">
+            <h1 >
+                STAR WARS FULL STACK PROJECT
+            </h1>
+        </header>
+    )
+}
+
+export default Header
