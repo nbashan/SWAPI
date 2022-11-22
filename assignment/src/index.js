f09e6c048972d471ad7b1a62032b9dfd6e6b7e08
נתנאל בשן <nbashan@g.jct.ac.il>
2022-11-22 20:25:54 +0200

    adding css file

--- assignment/src/index.js
+++ assignment/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { createRoot } from "react-dom/client";
 import SWAPI from './components/firstComponent';
+import './App.css';
 
 
 const rootElement = document.getElementById("root");
